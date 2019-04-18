0b095d9b0d09aff45b4db343f7095989639de7a6
fangwei2 <fangwei2@illinois.edu>
2019-04-18 08:56:36 -0500

    Updates

---
+++ precache-manifest.0f287794472546038d3d4c15357c5860.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c8657c4aee38141c576",
+    "url": "/instantler/static/css/main.67e34ba4.chunk.css"
+  },
+  {
+    "revision": "4c8657c4aee38141c576",
+    "url": "/instantler/static/js/main.4c8657c4.chunk.js"
+  },
+  {
+    "revision": "5c5570a55a5f379c1b9b",
+    "url": "/instantler/static/js/runtime~main.5c5570a5.js"
+  },
+  {
+    "revision": "8fe7afa6aa07ecde1b37",
+    "url": "/instantler/static/css/2.b91c378c.chunk.css"
+  },
+  {
+    "revision": "8fe7afa6aa07ecde1b37",
+    "url": "/instantler/static/js/2.8fe7afa6.chunk.js"
+  },
+  {
+    "revision": "252051bf4fce95657cdb397d4c3739e8",
+    "url": "/instantler/static/media/background.252051bf.jpg"
+  },
+  {
+    "revision": "a1a17ca01bb25db76a51d27f2fff5a4e",
+    "url": "/instantler/static/media/restHome.a1a17ca0.jpg"
+  },
+  {
+    "revision": "451d3df56a8ac44dca5fa2393aa7cdc7",
+    "url": "/instantler/index.html"
+  }
+];
